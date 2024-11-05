76699f46559560af21ec5063c6c5e8e557641089
Stina <stik@itu.dk>
2024-11-05 09:08:55 +0100

    node under construction

--- node/node.go
+++ node/node.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	proto "Consensus/grpc"
+	"sync"
+
+	"google.golang.org/grpc"
+	//"google.golang.org/grpc/credentials/insecure"
+)
+
+// Make nodes work as both client AND server #lifehack
+
 const (
 	held     string = "held"
 	released string = "released"
@@ -7,5 +17,23 @@ const (
 )
 
 type Node struct {
-	state string
+	proto.UnimplementedMutexServiceServer
+	state   string
+	nodeID  int32
+	replies chan int
+	mutex   sync.Mutex
+	// noget med channels
+}
+
+type Connection struct {
+	node           proto.MutexServiceClient
+	nodeConnection *grpc.ClientConn
+}
+
+func main() {
+
+	node := Node{
+		state: "released",
+	}
+
 }
